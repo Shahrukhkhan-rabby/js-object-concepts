5c31c245923d7f2e26c6b16beeaeb01f3827ed8f
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-18 17:41:37 +0600

    ses

--- api-checklist.js
+++ api-checklist.js
@@ -75,4 +75,72 @@ const divideByFive = numbers.map(num =>num/5);
 console.log(divideByFive);
 
 // 5. map foreach filter find
+// map
+const numbers1 = [1, 2, 3];
+const double = numbers1.map(num => num * 2);
+
+console.log(double);
+
+// forEach
+const numbers2 = [1, 2, 3, 4,];
+numbers2.forEach(num => console.log(num));
+
+// filter 
+const numbers3 = [1, 2, 3, 4];
+const evenNumbers3 = numbers3.filter(num => num % 2 === 0);
+
+console.log(evenNumbers3);
+
+// find
+const numbers4 = [1, 2, 3, 4];
+const firstEven = numbers4.find(num => num % 2 === 0);
+
+console.log(firstEven);
+
+// last even 
+const numbers5 = [1, 2, 3, 4, 5, 6];
+const evennumbers = numbers5.filter(num => num % 2 === 0);
+const lastEven = evennumbers.pop();
+
+console.log(lastEven);
+
+
 // 6. destructuring on object and array
+
+// Array Destructuring Syntax
+const [first, second9, third] = [1, 2, 3];
+console.log(first);
+console.log(second9);
+console.log(third);
+
+// Array Destructuring Example with default values:
+const [fourth, fifth = 2] = [1];
+console.log(fourth);
+console.log(fifth);
+
+
+// Array Destructuring skipping values
+
+const [, second] = [1, 2, 3];
+console.log(second);
+
+// Object Destructuring Syntax
+const { name, age1 } = { name: 'Mahfuz', age1: 33 }; 
+console.log(name);
+console.log(age1);
+
+// // Object Destructuring Renaming variables:
+const { name: fullName, age2: years } = { name: 'Maruf', age2: 43 };
+console.log(fullName);
+console.log(years);
+// // Object Destructuring Default values:
+const { name1, job = 'Engineer' } = { name1: 'Sohel' };
+console.log(name1);
+console.log(job);
+// // Object Destructuring Nested destructuring:
+const user = { name2: 'Sabbir', address: { city: 'Rajshahi' } };
+const { name2, address: { city } } = user;
+console.log(name2);
+console.log(city);
+
+
